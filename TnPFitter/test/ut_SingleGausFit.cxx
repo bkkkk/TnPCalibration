50ad7df1f845f9c7cf58b375619cf737354306b2
Jacobo Blanco <jayb88@cern.ch>
2013-08-20 15:14:59 +0100

    UNIT TEST FOR SINGLE GAUS FITTING FUNCTION
    Unit test based on the double gaussian equivalent. Initial commit of this with
    names changed. Shouldn't need any editing.

---
+++ TnPFitter/test/ut_SingleGausFit.cxx
@@ -0,0 +1,53 @@
+#include "TnPFitter/SingleGausFit.h"
+
+#include "TFile.h"
+#include "TH1F.h"
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+int main(int argc, char *argv[])
+{
+  int testStatus = 1;
+  
+  std::stringstream str;
+
+  str << argv[1];
+  int sigma = 3;
+  str >> sigma;
+
+  str.clear();
+  str.str(std::string());
+  str << argv[2];
+  int window = 2;
+  str >> window;
+
+  std::string filepath = "/scratch3/jblanco/CalibrationHistograms/dataSet_2013_07_16_16_51_02_PeriodB/hist-user.jayb88.20130625122121.data12_8TeV.periodB.physics_Muons.PhysCont.NTUP_SMWZ.grp13_v01_p1067.root";
+  std::string highBkgHistoName = "InvMass_eta_1.10_1.30_Probe";
+  
+  TFile* testFile = new TFile(filepath.c_str(), "OPEN");
+  if(testFile->IsZombie() == 1)
+  {
+    std::cout << "File could not be opened" << std::endl;
+  };
+
+  TH1F* histo = dynamic_cast<TH1F*> (testFile->Get(highBkgHistoName.c_str()));
+  
+  if(histo == NULL)
+  {
+    std::cout << "Histograms could not be opened" << std::endl;
+  };
+
+  SingleGausFit* fit = new SingleGausFit("probe", histo, TNPFITTER::BuildFitConfiguration(histo));
+  TNPFITTER::RunFit( fit );
+  TNPFITTER::DrawFit( fit, sigma, window );
+
+  double down = fit->GetBackgroundDownFunction()->GetParameter(0);
+  double up = fit->GetBackgroundUpFunction()->GetParameter(0);
+
+  std::cout << "Printing a variable from Up fit: " << up << std::endl;
+  std::cout << "Printing a variable from Down fit: " << down << std::endl;
+
+  return testStatus;
+}
