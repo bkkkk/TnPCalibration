b2fc85bde6d6b29889f935ac79ce08daff9466ed
Jacobo Blanco <jayb88@gmail.com>
2015-01-01 22:02:24 +0000

    Moved kinematic utils to gtest

--- TnPSelector/test/ut_KinematicUtils.cxx
+++ TnPSelector/test/ut_KinematicUtils.cxx
@@ -4,66 +4,65 @@
 
 #include <math.h>
 
-std::string TestKinematicUtils::comparison_msg(double expected, double returned) {
-    std::stringstream msgStream;
-    msgStream << "Expected: " << expected
-              << ", Returned: " << returned;
-    return msgStream.str();
+#include <gtest/gtest.h>
+
+TEST(GetEta, Positive) {
+  EXPECT_NEAR(0.88, TNP::GetEta(0.785398163), 0.01);
+  EXPECT_NEAR(2.44, TNP::GetEta(0.174532925), 0.01);
 }
 
-void TestKinematicUtils::test_assert_compare(double expected, double returned) {
-  TEST_ASSERT_MSG(expected == returned, comparison_msg(expected, returned).data());
-}
-
-void TestKinematicUtils::testGetEta() {
-  TEST_ASSERT_DELTA(0.88, TNP::GetEta(0.785398163), 0.01)
-  TEST_ASSERT_DELTA(2.44, TNP::GetEta(0.174532925), 0.01)
-}
-
-void TestKinematicUtils::testGetEtaThrowsOnZeroTheta() {
-  TEST_THROWS(TNP::GetEta(0), std::logic_error)
-}
-
-void TestKinematicUtils::testGetPt() {
-  test_assert_compare(0, TNP::GetPt(1, 0));
+TEST(GetPt, Positive) {  
+  EXPECT_EQ(1, TNP::GetPt(1, M_PI/2));
   
-  test_assert_compare(1, TNP::GetPt(1, M_PI/2));
-  
-  test_assert_compare(0.5, TNP::GetPt(2, M_PI/2));
+  EXPECT_EQ(0.5, TNP::GetPt(2, M_PI/2));
 }
 
-void TestKinematicUtils::testGetPtThrowsOnZeroQoverp() {
-  TEST_THROWS(TNP::GetPt(0, 0), std::logic_error);  
+TEST(GetPt, ZeroTheta) {
+  EXPECT_EQ(0, TNP::GetPt(1, 0));  
 }
 
-void TestKinematicUtils::testGetDeltaR() {
-  test_assert_compare(0, TNP::GetDeltaR(0, 0, 0, 0));
-
-  TEST_ASSERT_DELTA(sqrt(2), TNP::GetDeltaR(1, 1, 0, 0), 0.000001);
-
-  TEST_ASSERT_DELTA(1, TNP::GetDeltaR(1, 2 * M_PI, 0, 0), 0.000001);
+TEST(GetDeltaR, AllZero) {
+  EXPECT_EQ(0, TNP::GetDeltaR(0, 0, 0, 0));
 }
 
-void TestKinematicUtils::testGetDeltaZ0() {
-  test_assert_compare(1, TNP::GetDeltaZ0(-3, -2));
-  
-  test_assert_compare(1, TNP::GetDeltaZ0(2, 3));
-  test_assert_compare(1, TNP::GetDeltaZ0(3, 2));
-  
-  test_assert_compare(2, TNP::GetDeltaZ0(0, 2));
-  
-  test_assert_compare(4, TNP::GetDeltaZ0(2, -2));
-  test_assert_compare(4, TNP::GetDeltaZ0(-2, 2));
+TEST(GetDeltaR, Positive) {
+  EXPECT_FLOAT_EQ(sqrt(2), TNP::GetDeltaR(1, 1, 0, 0));
+  EXPECT_FLOAT_EQ(1, TNP::GetDeltaR(1, 2 * M_PI, 0, 0));
 }
 
-void TestKinematicUtils::testGetInvariantMass() {
+TEST(GetEta, ThrowsOnZeroTheta) {
+  EXPECT_THROW(TNP::GetEta(0), std::logic_error);
+}
+
+TEST(GetPt, ThrowsOnZeroQoverp) {
+  EXPECT_THROW(TNP::GetPt(0, 0), std::logic_error);  
+}
+
+TEST(GetDeltaZ0Test, Zero) {
+  EXPECT_EQ(2, TNP::GetDeltaZ0(0, 2));
+}
+
+TEST(GetDeltaZ0Test, AllNegative) {
+  EXPECT_EQ(1, TNP::GetDeltaZ0(-3, -2));
+}
+
+TEST(GetDeltaZ0Test, AllPositive) {
+  EXPECT_EQ(1, TNP::GetDeltaZ0(2, 3));
+  EXPECT_EQ(1, TNP::GetDeltaZ0(3, 2));
+}
+
+TEST(GetDeltaZ0Test, MixedSign) {
+  EXPECT_EQ(4, TNP::GetDeltaZ0(2, -2));
+  EXPECT_EQ(4, TNP::GetDeltaZ0(-2, 2));
+}
+
+TEST(GetInvariantMass, Nominal) {
   const double MuonMass = 105.6583715;
 
-  TEST_ASSERT_DELTA(MuonMass, TNP::GetInvariantMass(0, -2.44, 1, 0, 2.44, 2*M_PI, 0), 0.00001);
+  EXPECT_FLOAT_EQ(MuonMass, TNP::GetInvariantMass(0, -2.44, 1, 0, 2.44, 2*M_PI, 0));
 }
 
-int main(int argc, char const *argv[]) {
-  Test::TextOutput output(Test::TextOutput::Verbose);
-  TestKinematicUtils tku;
-  return (tku.run(output) ? EXIT_SUCCESS : EXIT_FAILURE);
+int main(int argc, char** argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return (RUN_ALL_TESTS());
 }
--- TnPSelector/test/ut_KinematicUtils.h
+++ TnPSelector/test/ut_KinematicUtils.h
@@ -1,35 +1,4 @@
 #ifndef KINEMATICUTILS_TEST_H_
 #define KINEMATICUTILS_TEST_H_ 1
 
-#include <cpptest.h>
-
-class TestKinematicUtils : public Test::Suite {
-public:
-  TestKinematicUtils() {
-    TEST_ADD(TestKinematicUtils::testGetEta)
-    TEST_ADD(TestKinematicUtils::testGetEtaThrowsOnZeroTheta)
-    TEST_ADD(TestKinematicUtils::testGetPt)
-    TEST_ADD(TestKinematicUtils::testGetPtThrowsOnZeroQoverp)
-    TEST_ADD(TestKinematicUtils::testGetDeltaR)
-    TEST_ADD(TestKinematicUtils::testGetDeltaZ0)
-    TEST_ADD(TestKinematicUtils::testGetInvariantMass)
-  }
-  
-private:
-  void testGetEta();
-  void testGetEtaThrowsOnZeroTheta();
-  
-  void testGetPt();
-  void testGetPtThrowsOnZeroQoverp();
-
-  void testGetDeltaR();
-  void testGetDeltaZ0();
-
-  void testGetInvariantMass();
-
-private:
-  std::string comparison_msg(double expected, double returned);
-  void test_assert_compare(double expected, double returned);
-};
-
 #endif
