ccc56cf8d70ad37ea7f887d4bfe88de532dc75b0
Jacobo Blanco <jayb88@gmail.com>
2015-01-02 18:47:18 +0000

    Initial reformatting of ChainNavigationTools

---
+++ D3PDReaderAdapter/D3PDReaderAdapter/ITruths.h
@@ -0,0 +1,18 @@
+#ifndef D3PDREADERADAPTER_ITRUTHS
+#define D3PDREADERADAPTER_ITRUTHS 1
+
+class ITruths
+{
+public:
+  ITruths();
+  ~ITruths(); 
+};
+
+class ITruth
+{
+public:
+  ITruth();
+  ~ITruth();
+};
+
+#endif
--- TruthNavigationTools/Root/ChainNavigationTools.cxx
+++ TruthNavigationTools/Root/ChainNavigationTools.cxx
@@ -1,102 +1,64 @@
 #include <string>
 #include <vector>
-#include "TruthNavigationTools/ChainNavigationTools.h"
-#include "JacobUtils/LoggingUtility.h"
 #include <iostream>
 
-//______________________________________________________________________________
-TT :: ChainNavigationTools ::
-ChainNavigationTools(const D3PDReader::TruthParticleD3PDObject& mc)
- : sink(mc)
-{ };
+#include "TruthNavigationTools/ChainNavigationTools.h"
 
-//______________________________________________________________________________
+TT::ChainNavigationTools::ChainNavigationTools(const D3PDReader::TruthParticleD3PDObject& mc)
+ : sink(mc) {
+}
 
-//______________________________________________________________________________
-void TT :: ChainNavigationTools ::
-PrintParentChain(const size_t index)
-{
+TT::ChainNavigationTools::~ChainNavigationTools() {
+}
+
+void TT::ChainNavigationTools::PrintParentChain(const size_t index) {
   TT::PrintParticleInformation(sink[index], true);
 
-  LOG_INFO() << "Index " << index << " which has " << sink[index].parent_index().size() << " parents";
-
-  for(size_t parentIdx = 0; parentIdx != sink[index].parent_index().size(); parentIdx++)
-  {
+  for(size_t parentIdx = 0; parentIdx != sink[index].parent_index().size(); parentIdx++) {
     PrintParentChain(sink[index].parent_index()[parentIdx]);
   }
-};
+}
 
-//______________________________________________________________________________
-void TT::ChainNavigationTools::PrintChildChain(const size_t index)
-{
+void TT::ChainNavigationTools::PrintChildChain(const size_t index){
   TT::PrintParticleInformation(sink[index], true);
 
-  LOG_INFO() << "Index " << index << " which has " << sink[index].child_index().size() << " children";
-
-  for(size_t childIdx = 0; childIdx != sink[index].child_index().size(); childIdx++)
-  {
+  for(size_t childIdx = 0; childIdx != sink[index].child_index().size(); childIdx++) {
     PrintChildChain(sink[index].child_index()[childIdx]);
   }
-};
+}
 
-//______________________________________________________________________________
-void TT :: ChainNavigationTools ::
-PrintSiblingChain(const size_t index, const siblingType type)
-{
+void TT::ChainNavigationTools::PrintSiblingChain(const size_t index, const siblingType type) {
   std::string siblingName = GetNameOfSibling(type);
-  
-  LOG_INFO() << "Printing chain of " << siblingName << " for index " << index;
-  
-
-  if(type == PARENT)
-  {
+    
+  if(type == PARENT) {
     PrintParentChain(index);
-  }
-  else
-  {
+  } else {
     PrintChildChain(index);
   }
-};
+}
 
-//______________________________________________________________________________
-void TT :: ChainNavigationTools ::
-PrintAllOfType(const int& pdgId)
-{
-  for(int mcIdx = 0; mcIdx != sink.n(); mcIdx++)
-  {
-    if(sink[mcIdx].pdgId() == pdgId || sink[mcIdx].pdgId() == -pdgId )
-    {
+void TT::ChainNavigationTools::PrintAllOfType(const int& pdgId) {
+  for(int mcIdx = 0; mcIdx != sink.n(); mcIdx++) {
+    if(sink[mcIdx].pdgId() == pdgId || sink[mcIdx].pdgId() == -pdgId ) {
       TT::PrintParticleInformation(sink[mcIdx]);
-    };
-  };
+    }
+  }
+}
 
-  return;
-};
-
-//______________________________________________________________________________
-std::vector<int> TT :: ChainNavigationTools ::
-FindAllOfType(const int& pdgId)
-{
+std::vector<int> TT::ChainNavigationTools::FindAllOfType(const int& pdgId) {
   std::vector<int> found;
 
-  for(int mcIdx = 0; mcIdx != sink.n(); mcIdx++)
-  {
-    if(sink[mcIdx].pdgId() == pdgId || sink[mcIdx].pdgId() == -pdgId )
-    {
+  for(int mcIdx = 0; mcIdx != sink.n(); mcIdx++) {
+    if(sink[mcIdx].pdgId() == pdgId || sink[mcIdx].pdgId() == -pdgId ) {
       found.push_back(mcIdx);
-    };
-  };
+    }
+  }
   
   return (found);
-};
+}
 
-//______________________________________________________________________________
-void TT :: ChainNavigationTools ::
-PrintAll()
-{
-  for(int mcIdx = 0; mcIdx!=sink.n(); mcIdx++)
-  {
+void TT::ChainNavigationTools::PrintAll() {
+  for(int mcIdx = 0; mcIdx!=sink.n(); mcIdx++) {
     TT::PrintParticleInformation(sink[mcIdx]);
-  };
-};
-
+  }
+}
