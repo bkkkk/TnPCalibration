57d3f4e1cc3787386c347bf0f689e3bf0e22968a
Jacobo Blanco <jayb88@gmail.com>
2015-01-09 14:27:20 +0000

    test files

---
+++ TruthNavigationTools/test/ut_TruthTools.h
@@ -0,0 +1,12 @@
+#ifndef TRUTHNAVIGATIONTOOLS_TRUTHTOOLS_H_
+#define TRUTHNAVIGATIONTOOLS_TRUTHTOOLS_H_ 1
+
+#include <gtest/gtest.h>
+
+class TestTruthTools : public ::testing::Test {
+protected:
+  TestTruthTools() { }
+  virtual ~TestTruthTools() { }
+};
+
+#endif
