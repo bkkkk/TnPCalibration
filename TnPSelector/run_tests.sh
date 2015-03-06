a982f00e90d92edb943e4a3b9cf4226fbe256c58
Jacobo Blanco <jayb88@gmail.com>
2015-03-06 10:51:10 +0000

    Added valgrind for thorough testing

--- TnPSelector/run_tests.sh
+++ TnPSelector/run_tests.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+VALGRIND=
+
 echo "Running unit tests:"
 
 for test_runner in test-bin/*
 do
   if test -f $test_runner
   then
-    if $test_runner
+    if $VALGRIND $test_runner 2>> test-bin/tests.log
     then
       echo $test_runner PASS
     else
