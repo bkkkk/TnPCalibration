9323bff7bc92bb3b8bf413ffd1ca34faf4aaccdf
Jacobo Blanco <jayb88@gmail.com>
2015-03-06 13:38:43 +0000

    Removed unnecessary function

--- TnPSelector/test/FakeMuons.h
+++ TnPSelector/test/FakeMuons.h
@@ -1,7 +1,9 @@
 #ifndef TNPSELECTOR_FAKEMUONS_H_
 #define TNPSELECTOR_FAKEMUONS_H_ 1
 
-#include <D3PDReaderAdapter/IMuons.h>
+#include "D3PDReaderAdapter/IMuons.h"
+
+#include "FakeMuon.h"
 
 class FakeMuons : public IMuons {
 public:
@@ -9,25 +11,26 @@ public:
   FakeMuons(std::initializer_list<FakeMuon> il)
    : muons{il} { }
 
+
   ~FakeMuons() {
     muons.clear();
   }
 
-  std::size_t n() {
+
+  std::size_t n() const {
     return (muons.size());
   }
 
-  const std::size_t n() const {
-    return (muons.size());
-  }
 
   IMuon& operator[](std::size_t idx) {
     return (muons[idx]);
   }
 
+
   const IMuon& operator[](std::size_t idx) const {
     return (muons[idx]);
   }
+
 
   void push_back(const FakeMuon& muon) {
     muons.push_back(muon);
