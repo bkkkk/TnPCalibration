8531247af42648399e635f6b8d6ff023896e66d2
Jacobo Blanco <jayb88@gmail.com>
2015-03-06 16:58:01 +0000

    Add test to check initialize passes when it should

--- TnPSelector/test/ut_PairSelector.cxx
+++ TnPSelector/test/ut_PairSelector.cxx
@@ -11,6 +11,17 @@ TEST_F(TestPairSelector, TestInitializeFailsForEmptySelector) {
   EXPECT_EQ(0, selector->initialize());
 }
 
+TEST_F(TestPairSelector, TestInitializePassForFilledSelector) {
+  TJPsiPairSelector* selector = new TJPsiPairSelector();
+  selector->deltaRCutMax = 0.1;
+  selector->deltaRCutMin = 1.5;
+  selector->signCut = -1;
+  selector->minMassCut = 1.5;
+  selector->maxMassCut = 4.2; 
+  selector->deltaZ0Cut = 0.1;
+  EXPECT_EQ(1, selector->initialize());
+}
+
 int main(int argc, char** argv) {
   ::testing::InitGoogleTest(&argc, argv);
   return (RUN_ALL_TESTS());
