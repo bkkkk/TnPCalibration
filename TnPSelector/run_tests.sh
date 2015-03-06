4df0b036a0dbcc95d9b77b502551125f48fceaad
Jacobo Blanco <jayb88@gmail.com>
2015-03-06 11:10:05 +0000

    Clean valgrind

--- TnPSelector/run_tests.sh
+++ TnPSelector/run_tests.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-VALGRIND=
-
 echo "Running unit tests:"
 
 for test_runner in test-bin/*
 do
   if test -f $test_runner
   then
-    if $VALGRIND $test_runner 2>> test-bin/tests.log
+    if $test_runner
     then
       echo $test_runner PASS
     else
