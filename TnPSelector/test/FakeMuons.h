fa9c6153433e4f9d943ea56ff3da09942e224ffd
Jacobo Blanco <jayb88@gmail.com>
2015-03-07 11:37:15 +0000

    Lint and autoformat

--- TnPSelector/test/FakeMuons.h
+++ TnPSelector/test/FakeMuons.h
@@ -1,46 +1,30 @@
 #ifndef TNPSELECTOR_FAKEMUONS_H_
 #define TNPSELECTOR_FAKEMUONS_H_ 1
 
+#include <vector>
+
 #include "D3PDReaderAdapter/IMuons.h"
 
-#include "FakeMuon.h"
+#include "./FakeMuon.h"
 
 class FakeMuons : public IMuons {
 public:
   FakeMuons() = default;
 
+  FakeMuons(std::initializer_list<FakeMuon> il) : muons{ il } {}
 
-  FakeMuons(std::initializer_list<FakeMuon> il)
-   : muons{il} { }
+  ~FakeMuons() { muons.clear(); }
 
+  std::size_t n() const { return (muons.size()); }
 
-  ~FakeMuons() {
-    muons.clear();
-  }
+  IMuon& operator[](std::size_t idx) { return (muons[idx]); }
 
+  const IMuon& operator[](std::size_t idx) const { return (muons[idx]); }
 
-  std::size_t n() const {
-    return (muons.size());
-  }
-
-
-  IMuon& operator[](std::size_t idx) {
-    return (muons[idx]);
-  }
-
-
-  const IMuon& operator[](std::size_t idx) const {
-    return (muons[idx]);
-  }
-
-
-  void push_back(const FakeMuon& muon) {
-    muons.push_back(muon);
-  }
+  void push_back(const FakeMuon& muon) { muons.push_back(muon); }
 
 private:
   std::vector<FakeMuon> muons;
 };
 
 #endif
-
