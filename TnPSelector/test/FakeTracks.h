81ea3d322786c11f662630ab5a1d9d4df103559e
Jacobo Blanco <jayb88@gmail.com>
2015-03-07 11:36:36 +0000

    Lint and autoformat

--- TnPSelector/test/FakeTracks.h
+++ TnPSelector/test/FakeTracks.h
@@ -1,43 +1,30 @@
 #ifndef TNPSELECTOR_FAKETRACKS_H_
 #define TNPSELECTOR_FAKETRACKS_H_ 1
 
+#include <vector>
+
 #include "D3PDReaderAdapter/ITrack.h"
 #include "D3PDReaderAdapter/ITracks.h"
-#include "FakeTrack.h"
+#include "./FakeTrack.h"
 
 class FakeTracks : public ITracks {
 public:
   FakeTracks() = default;
 
+  FakeTracks(std::initializer_list<FakeTrack> il) : tracks{ il } {}
 
-  FakeTracks(std::initializer_list<FakeTrack> il)
-   : tracks{il} { }
+  ~FakeTracks() { tracks.clear(); }
 
+  std::size_t n() const { return (tracks.size()); }
 
-  ~FakeTracks() {
-    tracks.clear();
-  }
+  ITrack& operator[](std::size_t idx) { return (tracks[idx]); }
 
+  const ITrack& operator[](std::size_t idx) const { return (tracks[idx]); }
 
-  std::size_t n() const {
-    return (tracks.size());
-  }
-
-  ITrack& operator[](std::size_t idx) {
-    return (tracks[idx]);
-  }
-
-  const ITrack& operator[](std::size_t idx) const {
-    return (tracks[idx]);
-  }
-
-  void push_back(const FakeTrack& Track) {
-    tracks.push_back(Track);
-  }
+  void push_back(const FakeTrack& Track) { tracks.push_back(Track); }
 
 private:
   std::vector<FakeTrack> tracks;
 };
 
 #endif
-
