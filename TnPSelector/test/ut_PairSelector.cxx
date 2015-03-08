768535197e5487d37671e42e4b70b39b87747687
Jacobo Blanco <jayb88@gmail.com>
2015-03-08 01:29:40 +0000

    Rename tests for cleaner output

--- TnPSelector/test/ut_PairSelector.cxx
+++ TnPSelector/test/ut_PairSelector.cxx
@@ -6,61 +6,60 @@
 #include "./FakeMuons.h"
 #include "./FakeMuon.h"
 
-TEST_F(TestPairSelector, TestInitializeFailsForEmptySelector) {
+TEST_F(TestPairSelector, InitializeFailsForEmptySelector) {
   TJPsiPairSelector* emptySelector = new TJPsiPairSelector();
   EXPECT_EQ(0, emptySelector->initialize());
 }
 
-TEST_F(TestPairSelector, TestInitializeFailsForBadlySetupDeltaRCuts) {
+TEST_F(TestPairSelector, InitializeFailsForBadlySetupDeltaRCuts) {
   selector->deltaRCutMax = 0.1;
   selector->deltaRCutMin = 1.5;
   EXPECT_EQ(0, selector->initialize());
 }
 
-TEST_F(TestPairSelector, TestInitializeFailsForBadInvariantMassRange) {
+TEST_F(TestPairSelector, InitializeFailsForBadInvariantMassRange) {
   selector->minMassCut = 0.4;
   selector->maxMassCut = 0.1;
   EXPECT_EQ(0, selector->initialize());
 }
 
-TEST_F(TestPairSelector, TestInitializeFailsForBadDeltaZCut) {
+TEST_F(TestPairSelector, InitializeFailsForBadDeltaZCut) {
   selector->deltaZ0Cut = 0.0;
   EXPECT_EQ(0, selector->initialize());
 }
 
-TEST_F(TestPairSelector, TestInitializePassForFilledSelector) {
+TEST_F(TestPairSelector, InitializePassForFilledSelector) {
   EXPECT_EQ(1, selector->initialize());
 }
 
-TEST_F(TestPairSelector, TestGoodPairPassesNumericInvariantMassCheck) {
+TEST_F(TestPairSelector, GoodPairPassesNumericInvariantMassCheck) {
   EXPECT_EQ(true, selector->isInInvariantMassRange(3140));
 }
 
-TEST_F(TestPairSelector, TestGoodPairPassesDeltaRCheck) {
+TEST_F(TestPairSelector, GoodPairPassesDeltaRCheck) {
   EXPECT_EQ(true, selector->isInDeltaRRange(1.0));
 }
 
-TEST_F(TestPairSelector, TestGoodPairPassesNumericAccept) {
+TEST_F(TestPairSelector, GoodPairPassesNumericAccept) {
   EXPECT_EQ(1, selector->accept(0.5, -1, 3140, 0.05));
 }
 
-TEST_F(TestPairSelector, TestBadPairFailsNumericAccept) {
+TEST_F(TestPairSelector, BadPairFailsNumericAccept) {
   EXPECT_EQ(0, selector->accept(100.0, -1, 3140, 0.05));
 }
 
-TEST_F(TestPairSelector, TestGoodProbePairPassesAccept) {
+TEST_F(TestPairSelector, GoodProbePairPassesAccept) {
   EXPECT_EQ(1, selector->accept(FakeMuon::ConstructGoodTagMuon(),
                                 FakeTrack::ConstructGoodProbe()));
 }
 
-TEST_F(TestPairSelector, TestBadProbePairFailsAccept) {
+TEST_F(TestPairSelector, BadProbePairFailsAccept) {
   EXPECT_EQ(0, selector->accept(FakeMuon::ConstructGoodTagMuon(),
                                 FakeTrack::ConstructBadProbe()));
 }
 
 /*
 int accept(const IMuon& tag, const IMuon& muonProbe);
-int accept(const IMuon& tag, const ITrack& probe);
 */
 
 int main(int argc, char** argv) {
