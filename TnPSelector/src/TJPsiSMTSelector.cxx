461d1d7e690453ddc8a6458504c456a9b1ae2331
Jacobo Blanco <jayb88@gmail.com>
2015-03-09 16:00:00 +0000

    Autoformat and lint

--- TnPSelector/src/TJPsiSMTSelector.cxx
+++ TnPSelector/src/TJPsiSMTSelector.cxx
@@ -2,27 +2,29 @@
 #include <TnPSelector/KinematicUtils.h>
 #include <TLorentzVector.h>
 #include <JacobUtils/LoggingUtility.h>
+#include <limits>
 
 TJPsiSMTSelector::TJPsiSMTSelector()
- : d0Cut(std::numeric_limits<float>::max()),
-   z0SinCut(std::numeric_limits<float>::max()),
-   chi2Cut(std::numeric_limits<float>::max()) {
-}
+    : d0Cut(std::numeric_limits<float>::max()),
+      z0SinCut(std::numeric_limits<float>::max()),
+      chi2Cut(std::numeric_limits<float>::max()) {}
 
-TJPsiSMTSelector::~TJPsiSMTSelector() {
-}
+TJPsiSMTSelector::~TJPsiSMTSelector() {}
 
 int TJPsiSMTSelector::initialize() {
-  if (d0Cut == std::numeric_limits<float>::max()) return (0);
-  if (z0SinCut == std::numeric_limits<float>::max()) return (0);
-  if (chi2Cut == std::numeric_limits<float>::max()) return (0);
+  if (d0Cut == std::numeric_limits<float>::max())
+    return (0);
+  if (z0SinCut == std::numeric_limits<float>::max())
+    return (0);
+  if (chi2Cut == std::numeric_limits<float>::max())
+    return (0);
 
   return (1);
 }
 
 int TJPsiSMTSelector::accept(const IMuon& muon) {
   auto d0 = muon.id_d0_exPV();
-  
+
   auto z0 = muon.id_z0_exPV();
   auto theta = muon.id_theta();
   auto z0sintheta = z0 * sin(theta);
@@ -33,12 +35,13 @@ int TJPsiSMTSelector::accept(const IMuon& muon) {
 }
 
 int TJPsiSMTSelector::accept(float d0, float z0sintheta, float chi2) {
-  if (fabs(d0) > d0Cut) return 0;
-  if (fabs(z0sintheta) > z0SinCut) return 0;
-  if (chi2 > chi2Cut) return 0;
+  if (fabs(d0) > d0Cut)
+    return 0;
+  if (fabs(z0sintheta) > z0SinCut)
+    return 0;
+  if (chi2 > chi2Cut)
+    return 0;
   return (1);
 }
 
-int TJPsiSMTSelector::finalize() {
-  return (1);
-}
+int TJPsiSMTSelector::finalize() { return (1); }
