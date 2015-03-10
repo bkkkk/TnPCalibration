ddd883b8f55cc93241b51d9fed8e7af7e19a7f96
Jacobo Blanco <jayb88@gmail.com>
2015-03-10 16:44:01 +0000

    Clean code, remove unnecessary comments and white spaces

--- SMTMiniNtuple/SMTMiniNtuple/MiniNtupleMaker.h
+++ SMTMiniNtuple/SMTMiniNtuple/MiniNtupleMaker.h
@@ -1,103 +1,84 @@
 #ifndef SMTMiniNtuple_MiniNtupleMaker_H
 #define SMTMiniNtuple_MiniNtupleMaker_H
 
-// Event Loop
 #include <EventLoop/Algorithm.h>
 #include <EventLoop/StatusCode.h>
 #include <EventLoopAlgs/NTupleSvc.h>
 
-/// Selectors
 #include <TnPSelector/TJPsiTagSelector.h>
 #include <TnPSelector/TMCPSelector.h>
 
-/// Trigger Selection
 #include <TrigRootAnalysis/TrigDecisionToolD3PD.h>
 #include <TnPSelector/TTriggerMatching.h>
 
-// Good Runs Lists
 #include <GoodRunsLists/TGoodRunsListReader.h>
 
-// D3PDReader
 #include <D3PDReader/Event.h>
 #include <D3PDReader/TrackParticleD3PDObject.h>
 #include <D3PDReader/triggerBitsD3PDObject.h>
 #include <D3PDReader/TrigMuonEFInfoD3PDObject.h>
 
-// Root
 #include <TH1F.h>
 
-namespace MIN
-{
-  class MiniNtupleMaker : public EL::Algorithm
-  {
-  public:
-    /// Ntuple Output Object
-    EL::NTupleSvc *output; //!
+namespace MIN {
+class MiniNtupleMaker : public EL::Algorithm {
+ public:
+  MiniNtupleMaker();
 
-  public:
-    /// Turn trigger cut on or off
-    bool doTriggerCut;
+ public:
+  virtual EL::StatusCode setupJob(EL::Job& job);
+  virtual EL::StatusCode histInitialize(void);
+  virtual EL::StatusCode changeInput(bool firstFile);
+  virtual EL::StatusCode initialize(void);
+  virtual EL::StatusCode execute(void);
+  virtual EL::StatusCode postExecute(void);
+  virtual EL::StatusCode finalize(void);
+  virtual EL::StatusCode histFinalize(void);
 
-  public:
-    /// List of triggers found
-    std::vector<std::string> triggersFound; //!
+ public:
+  EL::NTupleSvc* output;  //!
 
-  public:
-    /// Trigger Decision tool
-    D3PD::TrigDecisionToolD3PD* tdt; //!
+ public:
+  bool doTriggerCut;
 
-  public:
-    /// Output Name
-    std::string outputName;
+ public:
+  std::vector<std::string> triggersFound;  //!
 
-public:
-    /// Cutflow histogram
-    TH1F* cutFlow; //!
+ public:
+  D3PD::TrigDecisionToolD3PD* tdt;  //!
 
-  public:
-    /// Track object for thinning the collection
-    D3PDReader::TrackParticleD3PDObject out_tracks; //!
+ public:
+  std::string outputName;
 
-  public:
-    /// Event object
-    D3PDReader::Event *event; //!
+ public:
+  TH1F* cutFlow;  //!
 
-  public:
-    /// TGoodRunsList object
-    Root::TGoodRunsList my_grl;
+ public:
+  D3PDReader::TrackParticleD3PDObject out_tracks;  //!
 
-  public:
-    /// Tag selector
-    TJPsiTagSelector* tagSelector;
-    TMCPSelector* mcpSelector;
-    TTriggerMatching* triggerMatching;
-    
-  public:
-    std::vector<std::string> triggerList;
+ public:
+  D3PDReader::Event* event;  //!
 
-  public:
-    int eventCounter; //!
-    int eventDebug;
-    int eventInFile; //!
+ public:
+  Root::TGoodRunsList my_grl;
 
-  public:
-    // this is a standard constructor
-    MiniNtupleMaker ();
-  
-  public:
-    // these are the functions inherited from Algorithm
-    virtual EL::StatusCode setupJob (EL::Job& job);
-    virtual EL::StatusCode histInitialize (void);
-    virtual EL::StatusCode changeInput (bool firstFile);
-    virtual EL::StatusCode initialize (void);
-    virtual EL::StatusCode execute (void);
-    virtual EL::StatusCode postExecute (void);
-    virtual EL::StatusCode finalize (void);
-    virtual EL::StatusCode histFinalize (void);
-    
-    // this is needed to distribute the algorithm to the workers
-    ClassDef(MIN::MiniNtupleMaker, 1);
-  };
+ public:
+  TJPsiTagSelector* tagSelector;
+  TMCPSelector* mcpSelector;
+  TTriggerMatching* triggerMatching;
+
+ public:
+  std::vector<std::string> triggerList;
+
+ public:
+  int eventCounter;  //!
+  int eventDebug;
+  int eventInFile;  //!
+
+#ifdef __CINT__
+  ClassDef(MIN::MiniNtupleMaker, 1);
+#endif
+};
 };
 
 #endif
--- SMTMiniNtuple/src/MiniNtupleMaker.cxx
+++ SMTMiniNtuple/src/MiniNtupleMaker.cxx
@@ -1,20 +1,13 @@
-// Class definition
 #include <SMTMiniNtuple/MiniNtupleMaker.h>
-
-// Print out
 #include <iostream>
 
-// List maniupation
 #include <JacobUtils/ReaderTools.h>
 
-// Logging Utils
 #include <JacobUtils/LoggingUtility.h>
 
-// Event Loop
 #include <EventLoop/Job.h>
 #include <EventLoop/Worker.h>
 
-// ROOT Includes
 #include <TH1.h>
 #include <TLorentzVector.h>
 #include <TFile.h>
@@ -24,257 +17,206 @@ ClassImp(MIN::MiniNtupleMaker);
 
 namespace MIN {
 
-  MiniNtupleMaker ::
-  MiniNtupleMaker () :
-  doTriggerCut(true),
-  tagSelector(NULL),
-  mcpSelector(NULL),
-  triggerMatching(NULL),
-  eventDebug(20)
-  {
+MiniNtupleMaker::MiniNtupleMaker()
+    : doTriggerCut(true),
+      tagSelector(NULL),
+      mcpSelector(NULL),
+      triggerMatching(NULL),
+      eventDebug(20) {
+}
 
-  };
+EL::StatusCode MiniNtupleMaker::setupJob(EL::Job& job) {
+  job.useD3PDReader();
+  return EL::StatusCode::SUCCESS;
+}
 
-  // ============================================================================
+EL::StatusCode MiniNtupleMaker::histInitialize() {
+  cutFlow = new TH1F("Cutflow", ";Cut;Events", 11, -0.5, 5.5);
+  wk()->addOutput(cutFlow);
 
-  EL::StatusCode MiniNtupleMaker :: 
-  setupJob (EL::Job& job)
-  {
-    job.useD3PDReader();
-    return EL::StatusCode::SUCCESS;
-  };
+  return EL::StatusCode::SUCCESS;
+}
 
-  // ============================================================================
+EL::StatusCode MiniNtupleMaker::changeInput(bool firstFile) {
+  /// Reset in file event counter, used by trigger output system
+  eventInFile = 0;
 
-  EL::StatusCode MiniNtupleMaker ::
-  histInitialize ()
-  {
-    // Store histogram for cut flow
-    cutFlow = new TH1F("Cutflow",";Cut;Events", 11, -0.5, 5.5);
-    wk()->addOutput(cutFlow);
+  /// Get trigger information
+  TTree* trigConfTree =
+      dynamic_cast<TTree*>(wk()->inputFile()->Get("physicsMeta/TrigConfTree"));
+  TTree* eventTree = dynamic_cast<TTree*>(wk()->inputFile()->Get("physics"));
 
-    return EL::StatusCode::SUCCESS;
-  };
+  if ((eventTree == NULL) || (trigConfTree == NULL)) {
+    LOG_ERROR() << "Trigger Conf/Event Tree not present";
+    return EL::StatusCode::FAILURE;
+  } else {
+    LOG_INFO() << "Trees found";
+  }
 
-  // ============================================================================
+  LOG_INFO() << "Creating TrigDecisionTool";
 
-  EL::StatusCode MiniNtupleMaker ::
-  changeInput (bool firstFile)
-  {
-    (void) firstFile;
-
-    /// Reset in file event counter, used by trigger output system
-    eventInFile = 0;
-
-    /// Get trigger information
-    TTree* trigConfTree = dynamic_cast<TTree*> (wk()->inputFile()->Get("physicsMeta/TrigConfTree"));
-    TTree* eventTree = dynamic_cast<TTree*> (wk()->inputFile()->Get("physics"));
-
-    if((eventTree == NULL) || (trigConfTree == NULL))
-    {
-      LOG_ERROR() << "Trigger Conf/Event Tree not present";
-      return EL::StatusCode::FAILURE;
-    } else
-    {
-      LOG_INFO() << "Trees found";
-    }
-
-    LOG_INFO() << "Creating TrigDecisionTool";
-
-    if(firstFile != 1)
-    {
-      tdt = new D3PD::TrigDecisionToolD3PD(eventTree, trigConfTree);
-
-      /// Check which triggers are present
-      for(size_t trigIdx = 0; trigIdx != triggerList.size(); trigIdx++)
-      {
-        /// Add this trigger to a meta-data string
-        if(tdt->GetConfigSvc().IsConfigured(triggerList[trigIdx]) != 0)
-        {
-          LOG_DEBUG() << "Trigger " << triggerList[trigIdx] << " found";
-        };
-      };  
-    };
-
-    return EL::StatusCode::SUCCESS;
-  };
-
-  // ============================================================================
-
-  EL::StatusCode MiniNtupleMaker ::
-  initialize ()
-  {
-    /// Initialize debug
-    SET_LOG_LEVEL(kDEBUG4);
-    eventCounter = 0;
-
-    /// Make sure to set up D3PD reader
-    event = wk()->d3pdreader();
-
-    /// Get NTupleSvc
-    if (!outputName.empty())
-    {
-      output = EL::getNTupleSvc (wk(), outputName);
-    } else
-    {
-      output = 0;
-    };
-
-    TTree* trigConfTree = dynamic_cast<TTree*> (wk()->inputFile()->Get("physicsMeta/TrigConfTree"));
-    TTree* eventTree = dynamic_cast<TTree*> (wk()->inputFile()->Get("physics"));
-
+  if (firstFile != 1) {
     tdt = new D3PD::TrigDecisionToolD3PD(eventTree, trigConfTree);
 
     /// Check which triggers are present
-    for(size_t trigIdx = 0; trigIdx != triggerList.size(); trigIdx++)
-    {
+    for (size_t trigIdx = 0; trigIdx != triggerList.size(); trigIdx++) {
       /// Add this trigger to a meta-data string
-      if(tdt->GetConfigSvc().IsConfigured(triggerList[trigIdx]) != 0)
-      {
+      if (tdt->GetConfigSvc().IsConfigured(triggerList[trigIdx]) != 0) {
         LOG_DEBUG() << "Trigger " << triggerList[trigIdx] << " found";
-        output->copyBranch (triggerList[trigIdx]);
-        output->copyBranch ("trig_EF_trigmuonef_" + triggerList[trigIdx]);
-      };
-    };
+      }
+    }
+  }
 
-    /// Create a new track collection and add to output Ntuple
-    out_tracks.SetPrefix("trk_");
-    out_tracks.SetActive(kTRUE, "^trk_n$|^trk_pt$|^trk_eta$|^trk_phi_wrtPV$|^trk_chi2$|^trk_d0_wrtPV$|^trk_z0_wrtPV$|^trk_expectBLayerHit$|^trk_nBLHits$|^trk_nPixHits$|^trk_nPixHoles$|^trk_nPixelDeadSensors$|^trk_nSCTHits$|^trk_nSCTDeadSensors$|^trk_nSCTHoles$|^trk_nTRTHits$|^trk_nTRTOutliers$|^trk_ndof$|^trk_qoverp_wrtPV$|^trk_theta_wrtPV$");
-    if (output)
-    {
-      out_tracks.WriteTo(output->tree());
-    };
+  return (EL::StatusCode::SUCCESS);
+}
 
-    return EL::StatusCode::SUCCESS;
-  };
+EL::StatusCode MiniNtupleMaker::initialize() {
+  SET_LOG_LEVEL(kDEBUG4);
+  eventCounter = 0;
 
-  // ============================================================================
+  /// Make sure to set up D3PD reader
+  event = wk()->d3pdreader();
 
-  EL::StatusCode MiniNtupleMaker ::
-  execute ()
-  {
-    // Counter for tags
-    int nTags = 0;
+  /// Get NTupleSvc
+  if (!outputName.empty()) {
+    output = EL::getNTupleSvc(wk(), outputName);
+  } else {
+    output = 0;
+  }
 
-    // Cut counter
-    int cut = 0;
-    cutFlow->Fill(cut);
+  TTree* trigConfTree =
+      dynamic_cast<TTree*>(wk()->inputFile()->Get("physicsMeta/TrigConfTree"));
+  TTree* eventTree = dynamic_cast<TTree*>(wk()->inputFile()->Get("physics"));
 
-    // Turn on debug flag
-    if(eventCounter == eventDebug) SET_LOG_LEVEL(kWARNING);
-    LOG_DEBUG() << "Event in file: " << eventInFile;
+  tdt = new D3PD::TrigDecisionToolD3PD(eventTree, trigConfTree);
 
-    // Check if event is MC or passes GRL
-    if (!event->eventinfo.isSimulation())
-    {
-      int passGRL = my_grl.HasRunLumiBlock(event->eventinfo.RunNumber(),
-                                           event->eventinfo.lbn());
-      
-      /// Move to next event if it failed the GRL
-      if(passGRL != 1)
-      {
-        eventCounter++;
-        LOG_DEBUG() << "Event failed GRL";
-        return EL::StatusCode::SUCCESS;
-      };
+  /// Check which triggers are present
+  for (size_t trigIdx = 0; trigIdx != triggerList.size(); trigIdx++) {
+    /// Add this trigger to a meta-data string
+    if (tdt->GetConfigSvc().IsConfigured(triggerList[trigIdx]) != 0) {
+      LOG_DEBUG() << "Trigger " << triggerList[trigIdx] << " found";
+      output->copyBranch(triggerList[trigIdx]);
+      output->copyBranch("trig_EF_trigmuonef_" + triggerList[trigIdx]);
+    }
+  }
 
-      LOG_DEBUG() << "Event passed GRL";
+  /// Create a new track collection and add to output Ntuple
+  out_tracks.SetPrefix("trk_");
+  out_tracks.SetActive(kTRUE,
+                       "^trk_n$|^trk_pt$|^trk_eta$|^trk_phi_wrtPV$|^trk_chi2$|^"
+                       "trk_d0_wrtPV$|^trk_z0_wrtPV$|^trk_expectBLayerHit$|^"
+                       "trk_nBLHits$|^trk_nPixHits$|^trk_nPixHoles$|^trk_"
+                       "nPixelDeadSensors$|^trk_nSCTHits$|^trk_nSCTDeadSensors$"
+                       "|^trk_nSCTHoles$|^trk_nTRTHits$|^trk_nTRTOutliers$|^"
+                       "trk_ndof$|^trk_qoverp_wrtPV$|^trk_theta_wrtPV$");
+  if (output) {
+    out_tracks.WriteTo(output->tree());
+  }
 
-    } else
-    {
-      LOG_DEBUG() << "This MC";
-    };
-    ++cut;
-    cutFlow->Fill(cut);
-  
-    tdt->GetEntry(eventInFile);
-    
-    bool passedTrigger = false;
+  return EL::StatusCode::SUCCESS;
+}
 
-    for(size_t trigIdx = 0; trigIdx != triggerList.size(); trigIdx++)
-    {
-      if(tdt->GetConfigSvc().IsConfigured(triggerList[trigIdx]) == 1)
-      {
-        if(tdt->IsPassed( triggerList[trigIdx] ) != 0)
-        {
-          LOG_DEBUG() << "Event passes trigger: " << triggerList[trigIdx];
-          passedTrigger = true;
-        }; // End if trigger passed
-      }; // end if trigger configured
-    }; // end for trigger list
+EL::StatusCode MiniNtupleMaker::execute() {
+  // Counter for tags
+  int nTags = 0;
 
-    if(passedTrigger != 1 && doTriggerCut != 0)
-    {
-      LOG_DEBUG() << "Event hasn't passed a single trigger";
+  // Cut counter
+  int cut = 0;
+  cutFlow->Fill(cut);
+
+  // Turn on debug flag
+  if (eventCounter == eventDebug)
+    SET_LOG_LEVEL(kWARNING);
+  LOG_DEBUG() << "Event in file: " << eventInFile;
+
+  // Check if event is MC or passes GRL
+  if (!event->eventinfo.isSimulation()) {
+    int passGRL = my_grl.HasRunLumiBlock(event->eventinfo.RunNumber(),
+                                         event->eventinfo.lbn());
+
+    /// Move to next event if it failed the GRL
+    if (passGRL != 1) {
       eventCounter++;
-      eventInFile++;
+      LOG_DEBUG() << "Event failed GRL";
       return EL::StatusCode::SUCCESS;
-    };
+    }
 
+    LOG_DEBUG() << "Event passed GRL";
+
+  } else {
+    LOG_DEBUG() << "This MC";
+  }
+  ++cut;
+  cutFlow->Fill(cut);
+
+  tdt->GetEntry(eventInFile);
+
+  bool passedTrigger = false;
+
+  for (size_t trigIdx = 0; trigIdx != triggerList.size(); trigIdx++) {
+    if (tdt->GetConfigSvc().IsConfigured(triggerList[trigIdx]) == 1) {
+      if (tdt->IsPassed(triggerList[trigIdx]) != 0) {
+        LOG_DEBUG() << "Event passes trigger: " << triggerList[trigIdx];
+        passedTrigger = true;
+      }
+    }
+  }
+
+  if (passedTrigger != 1 && doTriggerCut != 0) {
+    LOG_DEBUG() << "Event hasn't passed a single trigger";
+    eventCounter++;
+    eventInFile++;
+    return EL::StatusCode::SUCCESS;
+  }
+
+  ++cut;
+  cutFlow->Fill(cut);
+
+  // Loop over muons to find tags
+  for (int muon = 0; muon != event->mu_staco.n(); ++muon) {
+    LOG_DEBUG() << "Current muon = " << muon + 1 << "/" << event->mu_staco.n();
+    if (tagSelector->accept(event->mu_staco[muon]) &&
+        mcpSelector->accept(event->mu_staco[muon]) &&
+        triggerMatching->accept(event->mu_staco[muon],
+                                event->trig_EF_trigmuonef)) {
+      LOG_DEBUG() << "This muon is a tag";
+      ++nTags;
+    }
+  }
+
+  out_tracks.Clear();
+
+  /// Loop over the tracks and remove the ones that DONT pass the MCP cuts
+  for (int track = 0; track != event->trk.n(); track++) {
+    if (mcpSelector->accept(event->trk[track])) {
+      out_tracks += event->trk[track];
+    }  // End if mcpSelector
+  }    // End for tracks
+
+  /// Require at least one tag
+  if (nTags != 0) {
     ++cut;
     cutFlow->Fill(cut);
+    LOG_DEBUG() << "There is at least one tag muon, event is written";
+    output->setFilterPassed();
+  }
 
-    // Loop over muons to find tags
-    for(int muon = 0; muon != event->mu_staco.n(); ++muon)
-    {
-      LOG_DEBUG() << "Current muon = " << muon + 1 << "/" << event->mu_staco.n();
-      if( tagSelector->accept(event->mu_staco[muon]) &&
-          mcpSelector->accept(event->mu_staco[muon]) &&
-          triggerMatching->accept(event->mu_staco[muon], event->trig_EF_trigmuonef ) )
-      {
-        LOG_DEBUG() << "This muon is a tag";
-        ++nTags;
-      }; // End if tagSelector
-    }; // End for muon
+  /// Count events
+  ++eventCounter;
+  ++eventInFile;
 
-    out_tracks.Clear();
+  return EL::StatusCode::SUCCESS;
+}
 
-    /// Loop over the tracks and remove the ones that DONT pass the MCP cuts
-    for(int track = 0; track != event->trk.n(); track++)
-    {
-      if(mcpSelector->accept(event->trk[track]))
-      {
-        out_tracks += event->trk[track];
-      }; // End if mcpSelector
-    }; // End for tracks
+EL::StatusCode MiniNtupleMaker::postExecute() {
+  return EL::StatusCode::SUCCESS;
+}
 
-    /// Require at least one tag
-    if(nTags != 0)
-    {
-      ++cut;
-      cutFlow->Fill(cut);
-      LOG_DEBUG() << "There is at least one tag muon, event is written";
-      output->setFilterPassed ();
-    }; // End if nTags!=0
+EL::StatusCode MiniNtupleMaker::finalize() {
+  return EL::StatusCode::SUCCESS;
+}
 
-    /// Count events
-    ++eventCounter;
-    ++eventInFile;
-
-    return EL::StatusCode::SUCCESS;
-  };
-
-  // ============================================================================
-
-  EL::StatusCode MiniNtupleMaker ::
-  postExecute ()
-  {
-    return EL::StatusCode::SUCCESS;
-  };
-
-  // ============================================================================
-
-  EL::StatusCode MiniNtupleMaker :: 
-  finalize ()
-  {
-    return EL::StatusCode::SUCCESS;
-  };
-
-  // ============================================================================
-
-  EL::StatusCode MiniNtupleMaker :: 
-  histFinalize ()
-  {
-    return EL::StatusCode::SUCCESS;
-  };
-};
+EL::StatusCode MiniNtupleMaker::histFinalize() {
+  return EL::StatusCode::SUCCESS;
+}
+}
