0c7ca16e47136ba3b12082495d93275f4ca3e9fa
Jacobo Blanco <jayb88@gmail.com>
2015-03-17 18:39:56 +0200

    Move construction of signal function to constructor

--- TnPFitter/src/SingleGausFit.cxx
+++ TnPFitter/src/SingleGausFit.cxx
@@ -21,22 +21,12 @@ void SingleGausFit::SetBackgroundFunction() {
 }
 
 void SingleGausFit::SetSignalFunction() {
-  testCompositeFunction();
-
-  auto fullFunctionName = functionName + "_signal_" + histogramName;
-  auto formula = fitConfig.GetSignalFitFunction();
-
-  signalFunction = new TF1(fullFunctionName.c_str(),
-                           formula.c_str(),
-                           bottomFitLimit,
-                           topFitLimit);
-
-  signalFunction->SetParNames("Gaus N", "Gaus Mean", "Gaus Sigma");
-
-  signalFunction->FixParameter(0, compositeFunction->GetParameter("Gaus N"));
-  signalFunction->FixParameter(1, compositeFunction->GetParameter("Gaus Mean"));
-  signalFunction->FixParameter(2,
-                               compositeFunction->GetParameter("Gaus Sigma"));
+  signalFunction->FixParameter(0,
+                               GetCompositeFunction()->GetParameter("Gaus N"));
+  signalFunction->FixParameter(
+      1, GetCompositeFunction()->GetParameter("Gaus Mean"));
+  signalFunction->FixParameter(
+      2, GetCompositeFunction()->GetParameter("Gaus Sigma"));
 }
 
 void SingleGausFit::SetCompositeUpFunction(void) {
