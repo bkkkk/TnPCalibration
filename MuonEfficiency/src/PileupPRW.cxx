7e7499fb2f67afa7e5fd0a62a42ffeb73fe518c2
Jacobo Blanco <jayb88@gmail.com>
2015-04-09 21:57:45 +0100

    Cleaned up CMake file

--- MuonEfficiency/src/PileupPRW.cxx
+++ MuonEfficiency/src/PileupPRW.cxx
@@ -3,92 +3,49 @@
 #include <EventLoop/Worker.h>
 #include <MuonEfficiency/PileupPRW.h>
 #include <EventLoop/OutputStream.h>
+#include <TFile.h>
 
-// this is needed to distribute the algorithm to the workers
-ClassImp(PileupPRW)
-
-
-
-PileupPRW :: PileupPRW () 
-: pileupTool(new Root::TPileupReweighting("pileupTool"))
-{
-  
+PileupPRW::PileupPRW()
+    : pileupTool(new Root::TPileupReweighting("pileupTool")) {
 }
 
-
-
-EL::StatusCode PileupPRW :: setupJob (EL::Job& job)
-{
+EL::StatusCode PileupPRW::setupJob(EL::Job& job) {
   job.useD3PDReader();
 
   EL::OutputStream out("outFile");
-  job.outputAdd( out );
+  job.outputAdd(out);
 
-  return EL::StatusCode::SUCCESS;
+  return (EL::StatusCode::SUCCESS);
 }
 
-
-
-EL::StatusCode PileupPRW :: histInitialize ()
-{
-  
-  return EL::StatusCode::SUCCESS;
-}
-
-
-
-EL::StatusCode PileupPRW :: changeInput (bool firstFile)
-{
-  return EL::StatusCode::SUCCESS;
-}
-
-
-
-EL::StatusCode PileupPRW :: initialize ()
-{
-  /// Make sure to set up D3PD reader
+EL::StatusCode PileupPRW::initialize() {
   event = wk()->d3pdreader();
 
   pileupTool->UsePeriodConfig("MC12a");
   pileupTool->Initialize();
 
-  return EL::StatusCode::SUCCESS;
+  return (EL::StatusCode::SUCCESS);
 }
 
+EL::StatusCode PileupPRW::execute() {
+  auto RunNumber = event->eventinfo.RunNumber();
+  auto mc_channel_number = event->eventinfo.mc_channel_number();
+  auto averageIntPerXing = event->eventinfo.averageIntPerXing();
 
-
-EL::StatusCode PileupPRW :: execute ()
-{
-  int RunNumber = event->eventinfo.RunNumber();
-  int mc_channel_number = event->eventinfo.mc_channel_number();
-  float averageIntPerXing = event->eventinfo.averageIntPerXing();
-  
-  pileupTool->Fill(RunNumber, mc_channel_number,(event->mcevt.weight())->at(0)[0],averageIntPerXing);
-  return EL::StatusCode::SUCCESS;
+  pileupTool->Fill(RunNumber,
+                   mc_channel_number,
+                   (event->mcevt.weight())->at(0)[0],
+                   averageIntPerXing);
+  return (EL::StatusCode::SUCCESS);
 }
 
-
-
-EL::StatusCode PileupPRW :: postExecute ()
-{
-  // Here you do everything that needs to be done after the main event
-  // processing.  This is typically very rare, particularly in user
-  // code.  It is mainly used in implementing the NTupleSvc.
-  return EL::StatusCode::SUCCESS;
-}
-
-
-
-EL::StatusCode PileupPRW :: finalize ()
-{
+EL::StatusCode PileupPRW::finalize() {
   TFile* f = wk()->getOutputFile("outFile");
   pileupTool->WriteToFile(f);
-  return EL::StatusCode::SUCCESS;
+
+  return (EL::StatusCode::SUCCESS);
 }
 
-
-
-EL::StatusCode PileupPRW :: histFinalize ()
-{
-  return EL::StatusCode::SUCCESS;
-}
+#ifdef __CINT__
+ClassImp(PileupPRW)
+#endif
